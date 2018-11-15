69e3efcd81902f4b981b4fabc020ee9347ab94b3
Joseph Radford <joeradford@gmail.com>
2018-11-15 21:05:54 +1100

    Hourglass sum solution

--- HackerRankSolutions/array_challenge.cpp
+++ HackerRankSolutions/array_challenge.cpp
@@ -1,9 +1,27 @@
 #include "stdafx.h"
 #include "array_challenge.h"
 
+#define ARR_SIZE 6
+
 using namespace std;
 // Complete the hourglassSum function below.
 int hourglassSum(std::vector<std::vector<int>> arr) {
-
-	return 0;
+	int max_result = 0;
+	bool max_result_set = false;
+	for (int row = 1; row < (ARR_SIZE - 1); row++) {
+		for (int col = 1; col < (ARR_SIZE - 1); col++) {
+			int result = arr[row - 1][col - 1] +
+						 arr[row - 1][col    ] +
+						 arr[row - 1][col + 1] +
+						 arr[row   ][col     ] +
+						 arr[row + 1][col - 1] +
+						 arr[row + 1][col    ] +
+						 arr[row + 1][col + 1];
+			if ((result > max_result) || !max_result_set) {
+				max_result = result;
+				max_result_set = true;
+			}
+		}
+	}
+	return max_result;
 }
